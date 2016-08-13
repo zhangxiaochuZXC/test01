80cd8fe99eb461ba0976e15f63729cb0548aee3e
ITManager <zhangjiesharp@163.com>
2016-08-13 17:50:27 +0800

    修改NSOperation的使用

--- 06-仿SDWebImage/06-仿SDWebImage/ViewController.m
+++ 06-仿SDWebImage/06-仿SDWebImage/ViewController.m
@@ -24,10 +24,7 @@
     
     // 实例化全局队列
     _queue = [[NSOperationQueue alloc] init];
-}
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
+    
     // 实例化自定义下载操作
     DownloadOperation *op = [[DownloadOperation alloc] init];
     // 添加到队列
